288a742e2b3cbd2a3120f67be5921c4f333fcaa9
Mike McNeil <mike@balderdash.co>
2014-12-16 09:57:28 -0600

    Update tests to NOT rely on the set-cookie res header (for security)

--- test/with-session.test.js
+++ test/with-session.test.js
@@ -7,6 +7,7 @@ var assert = require('assert');
 describe('with session', function (){
 
   before(function _setupRoutes(){
+
     sails.router.bind('PUT /me/jamiroquai', function (req, res){
       req.session.me = {
         id: 8,
@@ -14,25 +15,31 @@ describe('with session', function (){
       };
       res.send(200);
     });
+
     sails.router.bind('GET /me', function (req, res){
       res.send(req.session.me);
     });
   });
 
 
+  it('should not expose "set-cookie" response header', function (done){
+    io.socket.put('/me/jamiroquai', function (unused, jwr) {
+      if (jwr.headers['set-cookie']) {
+        return done(new Error('Should not expose "set-cookie" response header'));
+      }
+      return done();
+    });
+  });
+
+
+  it('should not expose all of the CORS response headers');
+
+
   it('should maintain session between requests', function (done){
 
     io.socket.put('/me/jamiroquai', function (unused, jwr) {
 
-      console.log('JWR from tests:',jwr);
-
-      io.socket.request({
-        method: 'GET',
-        url: '/me',
-        headers: {
-          cookie: jwr.headers['set-cookie']
-        }
-      },function (data) {
+      io.socket.get('/me', function (data) {
         assert.deepEqual(data, {
           id: 8,
           name: 'Jamiroquai'
