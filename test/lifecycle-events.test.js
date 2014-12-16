6b905419a335703dd987eca6ded4a74ed373954c
Mike McNeil <mike@balderdash.co>
2014-12-16 11:51:59 -0600

    onConnect tests pass.

--- test/lifecycle-events.test.js
+++ test/lifecycle-events.test.js
@@ -5,24 +5,69 @@
 var assert = require('assert');
 var util = require('util');
 
+var sails = require('sails');
+var socketioClient = require('socket.io-client');
+var sailsioClient = require('sails.io.js');
+
 
 describe('lifecycle events', function (){
 
+  // Used to check state below in tests
   var numTimesOnConnectTriggered = 0;
   var numTimesOnDisconnectTriggered = 0;
   var onConnectArgs;
   var onDisconnectArgs;
 
-  before(function (){
+  // Since we have to set up a separate app instance and io instance to test this,
+  // we just do that inline here
+  var io;
+  var app;
 
-    sails.config.sockets.onConnect = function (session, socket) {
-      numTimesOnConnectTriggered++;
-      onConnectArgs = Array.prototype.slice.call(arguments);
-    };
-    sails.config.sockets.onDisconnect = function (session, socket) {
-      numTimesOnDisconnectTriggered++;
-      onDisconnectArgs = Array.prototype.slice.call(arguments);
-    };
+  before(function (done){
+
+    // New up an instance of Sails and lift it.
+    app = sails.Sails();
+
+    app.lift({
+      port: 1684,
+      log: { level: 'error' },
+      globals: false,
+      hooks: {
+        // Inject the sockets hook in this repo into this Sails app
+        sockets: require('../')
+      },
+      loadHooks: ['moduleloader', 'userconfig', 'http', 'session', 'sockets'],
+      sockets: {
+        onConnect: function (session, socket) {
+          numTimesOnConnectTriggered++;
+          onConnectArgs = Array.prototype.slice.call(arguments);
+        },
+        onDisconnect: function (session, socket) {
+          numTimesOnDisconnectTriggered++;
+          onDisconnectArgs = Array.prototype.slice.call(arguments);
+        }
+      }
+    },function (err) {
+      if (err) return done(err);
+
+      // Instantiate socket client.
+      var client = sailsioClient(socketioClient);
+
+      // Globalize sails.io client as `io`
+      io = client;
+
+      // Set some options.
+      io.sails.url = 'http://localhost:'+1684;
+      io.sails.environment = 'production'; //(to disable logging)
+
+      return done(err);
+    });
+  });
+
+  after(function (done){
+    app.lower(function () {
+      done();
+    });
   });
 
 
