ab5b9e49484ad81900c98872081a65b1f81c6c32
Mike McNeil <mike@balderdash.co>
2014-12-17 08:41:55 -0600

    Eliminate need for origin header shadow route by plugging it into the receive-incoming-sails-io-msg script.

--- index.js
+++ index.js
@@ -14,21 +14,12 @@ module.exports = function (app){
       // Before the app's routes...
       before: {
 
-        'all /*': function addOriginHeader(req, res, next) {
-          if (req.isSocket) {
-            // TODO: pull this out - doesn't need to be here
-            req.headers = req.headers || {};
-            req.headers.origin = req.socket.handshake && req.socket.handshake.headers && req.socket.handshake.headers.origin;
-          }
-          return next();
-        }
-
       },
 
       // After the app's routes (i.e. if none matched)...
       after: {
 
-        // This shadow route can be disabled by setting:
+        // This "shadow" route can be disabled by setting:
         // `sails.config.sockets.grant3rdPartyCookie: false`
         'GET /__getcookie': function (req, res, next) {
           if (!sails.config.sockets.grant3rdPartyCookie) {
