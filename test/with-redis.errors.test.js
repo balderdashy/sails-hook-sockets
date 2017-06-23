9830e17f231f3d3744df6bc99f6f90676a6f7d77
Scott Gress <scottmgress@gmail.com>
2017-06-23 19:06:00 +0100

    Remove erroneous comment

--- test/with-redis.errors.test.js
+++ test/with-redis.errors.test.js
@@ -169,7 +169,6 @@ describe('with redis -- errors', function (){
         // Configure port to match .travis.yml
         port: 6380,
 
-        // Should fail, because by default Redis only has 16 dbs
         db: 2,
 
         // Configure password to match .travis.yml
